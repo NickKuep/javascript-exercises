55cae1b7ba1f55f17b1397a03dfa2fba27969726
nickkuep <nick_kuep@hotmail.com>
2022-11-17 13:44:29 -0500

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return string = "ERROR"
+        } else if (num == 0) {
+        return string = ""
+        } else for (let i = num; i > 0; i --) {
+        return string.repeat(num)
+        }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
