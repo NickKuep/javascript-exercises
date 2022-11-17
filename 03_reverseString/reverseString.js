d7c88169a9548a244c8c810c8da7f93397c87b76
nickkuep <nick_kuep@hotmail.com>
2022-11-17 13:56:13 -0500

    complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const newString = string.split('');
+    return newString.reverse().join('');
 };
 
 // Do not edit below this line
