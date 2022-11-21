7f20b6e4455516ffc723f60088747622d1646845
nickkuep <nick_kuep@hotmail.com>
2022-11-21 15:38:17 -0500

    complete leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 == 0) {
+    if (year % 400 && 100 != 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (year % 4 != 0) {
+    return false;
+  } else {
+    return true;
+  }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
