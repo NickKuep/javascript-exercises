fc84c7bd921e0a737ade165624ee54bff7444df8
nickkuep <nick_kuep@hotmail.com>
2022-11-21 16:04:38 -0500

    complete tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,16 @@
-const ftoc = function() {
-
+// x = (x - 32) 5/9
+const ftoc = function (fTemp) {
+  const celsius = (fTemp - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10;
 };
-
-const ctof = function() {
-
+// x = x(1.8)+32
+const ctof = function (cTemp) {
+  const fahr = cTemp * 1.8 + 32;
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
